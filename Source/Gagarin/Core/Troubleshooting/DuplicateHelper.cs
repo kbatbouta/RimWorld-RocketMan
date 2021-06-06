cb1258864e8155feee51581fbd483de2d6a52227
Karim Batbouta <kbatbouta66@gmail.com>
2021-06-05 20:48:27 -0700

    Added duplicated detection to GAGARIN

---
+++ Source/Gagarin/Core/Troubleshooting/DuplicateHelper.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Verse;
+
+namespace Gagarin
+{
+    public static class DuplicateHelper
+    {
+        private static DuplicateReport[] duplicates;
+
+        private static Dictionary<string, DuplicateReport> nameToReport = new Dictionary<string, DuplicateReport>();
+
+        private static Stopwatch stopwatch = new Stopwatch();
+
+        public static IEnumerable<DuplicateReport> Duplicates
+        {
+            get => duplicates;
+        }
+
+        public static void ParseCreateReports(XmlDocument document, Dictionary<XmlNode, LoadableXmlAsset> assetlookup)
+        {
+            stopwatch.Restart();
+            duplicates = Process(document, assetlookup)?.ToArray() ?? new DuplicateReport[0];
+            PrepareReportFolder();
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < duplicates.Length; i++)
+            {
+                DuplicateReport report = duplicates[i];
+                if (!report.HasDuplicates)
+                {
+                    throw new Exception($"GAGARIN:[DUPLICATE] Processor return a DuplicateReport with length={report.Length} for name={report.Name}");
+                }
+                int j = 1;
+                report.Write(Path.Combine(GagarinEnvironmentInfo.ReportsFolderPath, $"{report.Name}_{Rand.Int % 10000}.xml"));
+                builder.Clear();
+                builder.Append($"GAGARIN:[DUPLICATE] duplicate found for Name={report.Name}");
+                foreach (DuplicateReport.DuplicationRecord record in report.Records)
+                {
+                    builder.AppendInNewLine($"\t{j++}. PackageId={record.mod?.PackageId}\t| ModName={record.mod?.Name}\t| XmlFilePath={record.xmlFilePath}");
+                }
+                Log.Error(builder.ToString());
+            }
+            Log.Message($"GAGARIN:[DUPLICATE] Finished creating reports at <color=red>{GagarinEnvironmentInfo.ReportsFolderPath}</color>");
+            nameToReport.Clear();
+            stopwatch.Stop();
+            Log.Message($"GAGARIN:[DUPLICATE] Creating duplication reports took {stopwatch.ElapsedMilliseconds} MS");
+        }
+
+        private static IEnumerable<DuplicateReport> Process(XmlDocument document, Dictionary<XmlNode, LoadableXmlAsset> assetlookup)
+        {
+            foreach (XmlNode node in document.DocumentElement.ChildNodes)
+            {
+                if (node.NodeType != XmlNodeType.Element)
+                {
+                    continue;
+                }
+                XmlElement element = node as XmlElement;
+                if (false
+                    || !element.HasAttribute("Abstract")
+                    || !element.HasAttribute("Name")
+                    || element.GetAttribute("Abstract").ToLower() == "false")
+                {
+                    continue;
+                }
+                ModContentPack mod = Context.Core;
+                if (assetlookup.TryGetValue(node, out LoadableXmlAsset asset))
+                {
+                    mod = asset.mod;
+                }
+                string name = element.GetAttribute("Name");
+                if (!nameToReport.TryGetValue(name, out DuplicateReport report))
+                {
+                    report = nameToReport[name] = new DuplicateReport(name);
+                }
+                report.AddXmlNode(node, mod, asset?.FullFilePath ?? null);
+                if (report.HasDuplicates && report.Length == 2)
+                {
+                    yield return report;
+                }
+            }
+        }
+
+        private static void PrepareReportFolder()
+        {
+            if (!Directory.Exists(GagarinEnvironmentInfo.ReportsFolderPath))
+            {
+                Directory.Delete(GagarinEnvironmentInfo.ReportsFolderPath, recursive: true);
+            }
+            Directory.CreateDirectory(GagarinEnvironmentInfo.ReportsFolderPath);
+        }
+    }
+}
---
+++ Source/Gagarin/Core/Troubleshooting/DuplicateReport.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using Verse;
+
+namespace Gagarin
+{
+    public class DuplicateReport
+    {
+        private readonly string name;
+
+        private readonly List<DuplicationRecord> records = new List<DuplicationRecord>();
+
+        public struct DuplicationRecord
+        {
+            public string xmlFilePath;
+
+            public XmlNode node;
+
+            public ModContentPack mod;
+
+            public bool Parentless
+            {
+                get => xmlFilePath == null;
+            }
+
+            public static DuplicationRecord Invalid
+            {
+                get => new DuplicationRecord() { node = null, mod = null };
+            }
+
+            public static DuplicationRecord Create(XmlNode node, ModContentPack mod, string xmlFilePath)
+            {
+                return new DuplicationRecord() { mod = mod, node = node, xmlFilePath = xmlFilePath };
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj is DuplicationRecord other && other.mod == mod && other.node == node && other.xmlFilePath == xmlFilePath;
+            }
+
+            public override int GetHashCode()
+            {
+                return node.GetHashCode();
+            }
+        }
+
+        public string Name
+        {
+            get => name;
+        }
+
+        public int Length
+        {
+            get => records.Count;
+        }
+
+        public bool HasDuplicates
+        {
+            get => records.Count > 1;
+        }
+
+        public IEnumerable<DuplicationRecord> Records
+        {
+            get => records;
+        }
+
+        public IEnumerable<ModContentPack> CulpritMods
+        {
+            get => records.Select(r => r.mod);
+        }
+
+        public DuplicateReport(string name)
+        {
+            this.name = name;
+        }
+
+        public void AddXmlNode(XmlNode node, ModContentPack mod, string xmlFilePath)
+        {
+            records.Add(DuplicationRecord.Create(node, mod, xmlFilePath));
+        }
+
+        public ModContentPack GetCulprit(XmlNode node)
+        {
+            foreach (DuplicationRecord record in records)
+            {
+                if (record.node == node)
+                {
+                    return record.mod;
+                }
+            }
+            return null;
+        }
+
+        public void Write(string path)
+        {
+            XmlDocument document = new XmlDocument();
+            XmlElement root;
+            document.AppendChild(root = document.CreateElement("DuplicateReport"));
+            root.SetAttribute("Name", Name);
+            root.SetAttribute("Length", Length.ToString());
+            foreach (DuplicationRecord record in Records)
+            {
+                XmlElement node = document.CreateElement("DuplicationRecord");
+                node.SetAttribute("PackageId", record.mod?.PackageIdPlayerFacing);
+                node.SetAttribute("ModName", record.mod?.Name);
+                node.SetAttribute("XmlFilePath", record.xmlFilePath);
+                node.AppendChild(document.ImportNode(record.node, true));
+                root.AppendChild(node);
+            }
+            document.Save(path);
+        }
+
+        public override int GetHashCode()
+        {
+            return name.GetHashCode() ^ records.GetHashCode();
+        }
+    }
+}
