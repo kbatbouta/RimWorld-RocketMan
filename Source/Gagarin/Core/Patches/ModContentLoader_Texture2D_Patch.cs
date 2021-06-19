8da2f6e9e7f531f99765702ce8ccdebebcadb06f
DullSaurcer <kbatbouta66@gmail.com>
2021-06-19 14:08:38 -0700

    Texture caching improvement
    - Added a fallback mode incase of an error.

--- Source/Gagarin/Core/Patches/ModContentLoader_Texture2D_Patch.cs
+++ Source/Gagarin/Core/Patches/ModContentLoader_Texture2D_Patch.cs
@@ -19,46 +19,68 @@ namespace Gagarin
             }
 
             [HarmonyPriority(Priority.First)]
-            private static bool Load(VirtualFile file, ref Texture2D result)
+            private static bool Load(VirtualFile file, ref Texture2D result, bool fallbackMode = false)
             {
                 if (!file.Exists)
                 {
                     return true;
                 }
                 string binPath = GetBinTexturePath(file);
-                if (File.Exists(binPath))
+                if (File.Exists(binPath) && !fallbackMode)
                 {
-                    byte[] buffer = File.ReadAllBytes(binPath);
-                    byte[] data = new byte[buffer.Length - (4 + 4 + 4 + 1)];
-                    Array.Copy(buffer, 13, data, 0, data.Length);
-                    // This is the header
-                    // tex.width, tex.height, tex.format, tex.mipmapCount > 1);       
-                    result = new Texture2D(
-                        width: BitConverter.ToInt32(buffer, 0),
-                        height: BitConverter.ToInt32(buffer, 4),
-                        textureFormat: (TextureFormat)BitConverter.ToInt32(buffer, 8),
-                        mipChain: BitConverter.ToBoolean(buffer, 12)
-                    );
-                    result.LoadRawTextureData(data);
-                    result.name = Path.GetFileNameWithoutExtension(file.Name);
-                    result.filterMode = FilterMode.Trilinear;
-                    result.Apply();
+                    try
+                    {
+                        byte[] buffer = File.ReadAllBytes(binPath);
+                        byte[] data = new byte[buffer.Length - (4 + 4 + 4 + 1)];
+                        Array.Copy(buffer, 13, data, 0, data.Length);
+
+                        // This is the header
+                        // tex.width, tex.height, tex.format, tex.mipmapCount > 1);       
+                        result = new Texture2D(
+                            width: BitConverter.ToInt32(buffer, 0),
+                            height: BitConverter.ToInt32(buffer, 4),
+                            textureFormat: (TextureFormat)BitConverter.ToInt32(buffer, 8),
+                            mipChain: BitConverter.ToBoolean(buffer, 12)
+                        );
+                        result.LoadRawTextureData(data);
+                        result.name = Path.GetFileNameWithoutExtension(file.Name);
+                        result.filterMode = FilterMode.Trilinear;
+                        result.Apply();
+                    }
+                    catch (Exception er)
+                    {
+                        RocketMan.Logger.Debug($"GAGARIN: Error loading texture! using fallback mode!", exception: er);
+                        if (File.Exists(binPath))
+                        {
+                            File.Delete(binPath);
+                        }
+                        return Load(file, ref result, fallbackMode: true);
+                    }
                 }
                 else
                 {
-                    result = LoadTexture(file);
-                    // This is the header
-                    // tex.width, tex.height, tex.format, tex.mipmapCount > 1);                    
-                    byte[] data = result.GetRawTextureData();
-                    byte[] buffer = new byte[data.Length + 4 + 4 + 4 + 1];
-                    BitConverter.GetBytes(result.width).CopyTo(buffer, 0);
-                    BitConverter.GetBytes(result.height).CopyTo(buffer, 4);
-                    BitConverter.GetBytes((int)result.format).CopyTo(buffer, 8);
-                    BitConverter.GetBytes(result.mipmapCount > 1).CopyTo(buffer, 12);
-                    data.CopyTo(buffer, 13);
+                    try
+                    {
+                        result = LoadTexture(file);
 
-                    File.WriteAllBytes(binPath, buffer);
-                    result.Apply(updateMipmaps: false, makeNoLongerReadable: true);
+                        // This is the header
+                        // tex.width, tex.height, tex.format, tex.mipmapCount > 1);                    
+                        byte[] data = result.GetRawTextureData();
+                        byte[] buffer = new byte[data.Length + 4 + 4 + 4 + 1];
+                        BitConverter.GetBytes(result.width).CopyTo(buffer, 0);
+                        BitConverter.GetBytes(result.height).CopyTo(buffer, 4);
+                        BitConverter.GetBytes((int)result.format).CopyTo(buffer, 8);
+                        BitConverter.GetBytes(result.mipmapCount > 1).CopyTo(buffer, 12);
+                        data.CopyTo(buffer, 13);
+
+                        File.WriteAllBytes(binPath, buffer);
+                        result.Apply(updateMipmaps: false, makeNoLongerReadable: true);
+                    }
+                    catch (Exception er)
+                    {
+                        RocketMan.Logger.Debug($"GAGARIN: Error creating texture cache! skipping this one!", exception: er);
+                        return true;
+                    }
                 }
                 return false;
             }
